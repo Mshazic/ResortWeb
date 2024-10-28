c684850dd98b6c843d6c64335f7c139331c0ff22
senzonzuza <senzokaqhaka@gmail.com>
2024-10-28 14:22:13 +0200

    sln

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ResortWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ ResortWeb.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class ResortWeb : Controller
+    {
+        // GET: ResortWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
